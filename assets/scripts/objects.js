a3b014eba37090b774ea523a228f805bbdc89ae4
Satadru Roy <satadru.roy98@gmail.com>
2020-07-14 03:16:59 +0530

    -"this" and Arrow Functions

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -62,7 +62,7 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
-const searchMovieHandler = function() {
+const searchMovieHandler = () => {
   console.log(this);
   const filterTerm = document.getElementById('filter-title').value;
   renderMovies(filterTerm);
